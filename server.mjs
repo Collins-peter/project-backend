fa0c4c59ecaf9966702f2fe2d709b173766a2898
Collins-peter <anicollins09@gmail.com>
2026-01-09 12:55:08 +0100

    updated VITE_URL

--- server.mjs
+++ server.mjs
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import db from "./Database/dataBase.mjs";
@@ -10,20 +8,22 @@ import createPackage from "./controller/createPackage.mjs";
 import trackPackage from "./controller/trackPackage.mjs";
 import updateStatus from "./controller/updateStatus.mjs";
 
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin : "http://localhost:5173",
+        origin : process.env.API_VITE_URL,
         methods : ["GET", "POST"]
     }
 })
 
 //CONNECTING FRONTEND URL
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.API_VITE_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
